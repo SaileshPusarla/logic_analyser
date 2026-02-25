7aa1d06f6e6dc9ceb3983b938abd0ec9329f45ad
Manjunatha Sarma Majety <manjunathasarmamajety@gmail.com>
2026-02-25 21:51:25 +0530

    Add submodule pico-sdk to simplify library usage, Add cmakelists.txt and a simple hello world program

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include "pico/stdlib.h"
 
+int main() {
+    // 1. Initialize all standard IO (enables USB/UART)
+    stdio_init_all();
 
-int main(){
+    // 2. Initialize the onboard LED
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    printf("hello world");
+    while (true) {
+        // Blink the LED
+        gpio_put(LED_PIN, 1);
+        printf("Hello from Pico! LED is ON\n");
+        sleep_ms(500);
+
+        gpio_put(LED_PIN, 0);
+        printf("Hello from Pico! LED is OFF\n");
+        sleep_ms(500);
+    }
 }
